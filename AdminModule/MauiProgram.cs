006bd2f2abcb3ca4ae23f3344d44dffd15c98871
max <max@gmail.com>
2024-05-11 15:41:16 +0300

    Upadate for Acceptor AdminModule

--- AdminModule/MauiProgram.cs
+++ AdminModule/MauiProgram.cs
@@ -1,7 +1,5 @@
 ﻿using AdminModule.Resources.Interfaces;
-using AdminModule.Resources.ViewModels;
 using AdminModule.Resources.Views;
-using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 
 namespace AdminModule
@@ -13,26 +11,22 @@ namespace AdminModule
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-            if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
-            {
-                builder.Services.AddSingleton<ILoginPage, LoginPage>();
-                builder.Services.AddSingleton<ISignupPage, SignupPage>();
-                builder.Services.AddSingleton<IAdminDashboardPage, AdminDashboardPage>();
-                builder.Services.AddSingleton<IAircraftPage, AircraftPage>();
-                builder.Services.AddSingleton<ILocationAndAirportPage, LocationAndAirportPage>();
+            builder.Services.AddSingleton<ILoginPage, LoginPage>();
+            builder.Services.AddSingleton<ISignupPage, SignupPage>();
+            builder.Services.AddSingleton<IAdminDashboardPage, AdminDashboardPage>();
+            builder.Services.AddSingleton<IAircraftPage, AircraftPage>();
+            builder.Services.AddSingleton<ILocationAndAirportPage, LocationAndAirportPage>();
 
-                builder.Services.AddTransient<IAddOrEditAircraftPage, AddOrEditAircraftPage>();
-            }
+            builder.Services.AddTransient<IAddOrEditAircraftPage, AddOrEditAircraftPage>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
